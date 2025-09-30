c6d71439ebb123f32c4045c73e7f42b9767f2c1e
Rapa0 <153472756+Rapa0@users.noreply.github.com>
2025-09-30 18:16:22 -0400

    Update sendEmail.js

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,25 +1,33 @@
 const nodemailer = require('nodemailer');
 
-const sendEmail = async options => {
+const sendEmail = async (options) => {
 
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
     auth: {
       user: process.env.EMAIL_USER, 
       pass: process.env.EMAIL_PASS,
     },
   });
 
+ 
   const mailOptions = {
     from: 'RecetasApp <no-reply@recetasapp.com>',
     to: options.email,
     subject: options.subject,
     text: options.message,
-
   };
 
 
-  await transporter.sendMail(mailOptions);
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Correo enviado con éxito:', info.response);
+  } catch (error) {
+    console.error('Error al enviar el correo:', error);
+
+    throw new Error('No se pudo enviar el email de verificación.');
+  }
 };
 
 module.exports = sendEmail;
